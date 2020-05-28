277acdedc694749c5ca89d17ce180ff94f155400
Salva Cardona <salvacm99@hotmail.es>
2020-05-28 18:02:37 +0200

    subo 4a act

---
+++ 4a act API rest/API.js
@@ -0,0 +1,6 @@
+Lo primero, siempre, intenta hacer el layout de la interfaz y poner todas las cosas en su sitio, aunque sea con datos inventados.
+Cuando tengas eso, empieza con los listeners y déjalos planteados quizá con llamadas a window.alert.
+Cuando tengas hasta ese punto, lo verás mucho más claro. Desde la URL que os he puesto, descarga la información.
+Ese es el JSON que vais a tener que procesar, ya sea de forma estática o de forma dinámica en la opción 2.
+Intenta siempre planteártelo dividiendo el tema grande en temas pequeños.
+Empieza con lo que te comento y verás como cuando tengas la pantalla, lo ves más claro.
